8513c8bcdc2c1b26383ef39a3583a66220c35639
nicolas-wai <nicolaswai03@gmail.com>
2026-08-20 09:43:06 -0300

    Agregue el form

--- main.go
+++ main.go
@@ -5,8 +5,33 @@ import (
 	"net/http"
 )
 
+func serveOpinion(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/opinion" || r.Method != http.MethodPost {
+		http.NotFound(w, r)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Error al parsear", http.StatusBadRequest)
+	}
+	name := r.FormValue("name")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<!DOCTYPE html>
+						<html lang="es">
+						<head>
+							<meta charset="UTF-8">
+							<meta name="viewport" content="width=device-width, initial-scale=1.0">
+							<title>Document</title>
+						</head>
+						<body>
+							<h1>Gracias por tu opinion, %s!</h1>
+							<a href="/">Volver</a>
+						</body>
+						</html>`,
+		name)
+}
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
+	if r.URL.Path != "/" || r.Method != http.MethodGet {
 		http.NotFound(w, r)
 		return
 	}
@@ -20,6 +45,8 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fileServer)) // Preguntar: que es StripPrefix
 
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/opinion", serveOpinion)
+
 	port := ":8080"
 	fmt.Printf("Server running localhost%s", port)
 	err := http.ListenAndServe(port, nil)
